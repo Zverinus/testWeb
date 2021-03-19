ba14646fc5dd391ec4c44c82756cd067bdec7c44
Zverinus <presentdragons@yandex.ru>
2021-03-19 17:17:55 +0300

    3rd commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+# import os
+
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
@@ -10,5 +11,5 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=5000)
